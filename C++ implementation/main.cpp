2a33654477a87eb370701a350b9a93692f84b300
TysonKeilty <105589295+TysonKeilty@users.noreply.github.com>
2022-06-14 16:51:13 +0200

    Update main.cpp

--- C++ implementation/main.cpp
+++ C++ implementation/main.cpp
@@ -15,18 +15,21 @@ using namespace std;
 
 // Convert File into string
 string readFileIntoString(const string& path) {
+    
     ifstream input_file(path);
+	
     if (!input_file.is_open()) {
         cerr << "Could not open the file - '"
              << path << "'" << endl;
         exit(EXIT_FAILURE);
     }
+	
     return string((istreambuf_iterator<char>(input_file)), istreambuf_iterator<char>());
 }
 
 int main(int argc, char** argv){
-	
-	string file_contents;
+
+    string file_contents;
     string file_name=argv[1];
 	
     cout << "Reading file..." << endl;
